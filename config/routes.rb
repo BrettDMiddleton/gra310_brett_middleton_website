f941ca951c588d93cfa975b210d08487eb3d2873
Brett Middleton <brett01101101@gmail.com>
2018-10-07 22:28:08 -0400

    navigation

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,6 @@ Rails.application.routes.draw do
   root to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get 'sitemap', to: 'pages#sitemap', as: :sitemap
+  get 'menu', to: 'pages#menu', as: :menu
+  get 'locations', to: 'pages#locations', as: :locations
 end
